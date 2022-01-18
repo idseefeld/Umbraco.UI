c5693f81f3089e3ede694591f6c80128281ada13
Niels Lyngsø <niels.lyngso@gmail.com>
2022-01-18 21:03:52 +0100

    typings and map

--- packages/uui-icon/lib/define.ts
+++ packages/uui-icon/lib/define.ts
@@ -3,3 +3,9 @@ import { defineElement } from '@umbraco-ui/uui-base/lib/registration';
 
 defineElement('uui-icon', UUIIconElement as any);
 //TODO: consider if we should ship a define method from index and then that is the method called if importing define.
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'uui-icon': UUIIconElement;
+  }
+}
